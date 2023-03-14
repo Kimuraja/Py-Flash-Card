183b40f7694fe87cff732aad717f4f03ba01e26e
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-03-14 15:49:29 +0100

    Fixed buttons
    Buttons now works properly !

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import random
 from tkinter import *
 from random import *
 import pandas as pd
@@ -6,23 +5,25 @@ import pandas as pd
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- FLASH CARDS ------------------------------- #
-
 df = pd.read_csv("data/Korea.csv")
 df.to_dict(orient="records")
-ko_alp = choice(df.values)
-
 
 def guess():
+    global translation
+    rand = choice(df.values)
+    word = rand[0]
+    translation = rand[1]
     canvas.itemconfig(canvas_image, image=front_img)
     canvas.delete("txt")
-    canvas.create_text(400, 263, text=f"{ko_alp[0]}", font=("Ariel", 60, "bold"), tags="txt")
-
+    canvas.create_text(400, 263, text=f"{word}", font=("Ariel", 60, "bold"), tags="txt")
+    window.after(3000, swap_bg)
 
 def swap_bg():
+
     canvas.itemconfig(canvas_image, image=back_img)
     canvas.delete("txt")
-    canvas.create_text(400, 263, text=f"{ko_alp[1]}", font=("Ariel", 60, "bold"), tags="txt")
-    window.after_cancel(swap)
+    canvas.create_text(400, 263, text=f"{translation}", font=("Ariel", 60, "bold"), tags="txt")
+    # window.after_cancel(swap)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -41,7 +42,7 @@ right_button = Button(image=right, highlightthickness=0, command=guess)
 right_button.grid(column=1, row=2)
 
 wrong = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong, highlightthickness=0, command=swap_bg)
+wrong_button = Button(image=wrong, highlightthickness=0, command=guess)
 wrong_button.grid(column=0, row=2)
 
 
@@ -52,7 +53,5 @@ canvas.create_text(400, 150, text="Korean", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 315, text="For latin speakers", font=("Ariel", 13, "bold"))
 canvas.grid(column=0, row=0, columnspan=2, rowspan=2)
 
-
-swap = window.after(3000, swap_bg)
 
 mainloop()
