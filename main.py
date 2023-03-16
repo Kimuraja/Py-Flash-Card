3c95e3debe6b5732936fe67d5ae5ba527dc01fe2
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-03-16 19:41:42 +0100

    Grabage push
    Green button works totally fine but red one do not deletes list element, also I have to figure out how to list properly csv file

--- main.py
+++ main.py
@@ -8,26 +8,36 @@ BACKGROUND_COLOR = "#B1DDC6"
 df = pd.read_csv("data/Korea.csv")
 df.to_dict(orient="records")
 
-def guess():
-    global translation
-    rand = choice(df.values)
-    word = rand[0]
-    translation = rand[1]
+# I'm not rly sure if df.values ig a good option for listing csv file
+rand = choice(df.values)
+
+
+def good_answer():
+    global rand_list
+    global random_choice
+    random_choice = choice(df.values)
+    rand_list = list(random_choice)
     canvas.itemconfig(canvas_image, image=front_img)
     canvas.delete("txt")
     canvas.delete("enter")
+    canvas.delete("eng")
     canvas.create_text(400, 150, text="Korean", font=("Ariel", 50, "italic"), tags="black_title", fill="black")
-    canvas.create_text(400, 263, text=f"{word}", font=("Ariel", 70, "bold"), tags="txt", fill="black")
+    canvas.create_text(400, 263, text=f"{rand_list[0]}", font=("Ariel", 70, "bold"), tags="txt", fill="black")
     canvas.create_text(400, 330, text="For latin speakers", font=("Ariel", 15, "bold"), tags="latin", fill="black")
     window.after(3000, swap_bg)
 
+def bad_aswer():
+    rand_list.remove(random_choice)
+    good_answer()
 
 def swap_bg():
     canvas.delete("black_title")
     canvas.itemconfig(canvas_image, image=back_img)
     canvas.delete("txt")
     canvas.delete("latin")
-    canvas.create_text(400, 263, text=f"{translation}", font=("Ariel", 60, "bold"), tags="txt")
+    canvas.create_text(400, 150, text="English", font=("Ariel", 50, "italic"), tags="eng", fill="white")
+    canvas.create_text(400, 263, text=f"{rand_list[1]}", font=("Ariel", 60, "bold"), tags="txt", fill="white")
+
     # window.after_cancel(swap)
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -41,15 +51,14 @@ front_img = PhotoImage(file="images/card_front.png")
 back_img = PhotoImage(file="images/card_back.png")
 
 
-
 # ---------- BUTTONS ---------- #
 
 right = PhotoImage(file="images/right.png")
-right_button = Button(image=right, highlightthickness=0, command=guess)
+right_button = Button(image=right, highlightthickness=0, command=good_answer)
 right_button.grid(column=1, row=2)
 
 wrong = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong, highlightthickness=0, command=guess)
+wrong_button = Button(image=wrong, highlightthickness=0, command=bad_aswer)
 wrong_button.grid(column=0, row=2)
 
 
@@ -57,7 +66,6 @@ canvas = Canvas(width=800, height=526)
 canvas_image = canvas.create_image(400, 263, image=front_img)
 canvas.configure(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_text(400, 230, text="Click any button to start", font=("Ariel", 40, "bold"), tags="enter", fill="black")
-canvas.create_text(400, 150, text="English", font=("Ariel", 50, "italic"), tags="title")
 canvas.grid(column=0, row=0, columnspan=2, rowspan=2)
 
 
