6930c49e0469323fb5c0fff13101c412fa5ca4b0
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-03-17 08:08:36 +0100

    HOTFIX #1

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from tkinter import *
 from random import *
 import pandas as pd
-# import time
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- FLASH CARDS ------------------------------- #
 df = pd.read_csv("data/Korea.csv")
-df.to_dict(orient="records")
+my_list = [i for i in df.to_dict(orient="records")]
 
-# I'm not rly sure if df.values ig a good option for listing csv file
-rand = choice(df.values)
+
+def random_select():
+    global random_choice
+    random_choice = choice(my_list)
 
 
 def good_answer():
-    global rand_list
-    global random_choice
-    random_choice = choice(df.values)
-    rand_list = list(random_choice)
     canvas.itemconfig(canvas_image, image=front_img)
     canvas.delete("txt")
     canvas.delete("enter")
     canvas.delete("eng")
     canvas.create_text(400, 150, text="Korean", font=("Ariel", 50, "italic"), tags="black_title", fill="black")
-    canvas.create_text(400, 263, text=f"{rand_list[0]}", font=("Ariel", 70, "bold"), tags="txt", fill="black")
+    canvas.create_text(400, 263, text=f"{random_choice['symbols']}", font=("Ariel", 70, "bold"), tags="txt",
+                       fill="black")
     canvas.create_text(400, 330, text="For latin speakers", font=("Ariel", 15, "bold"), tags="latin", fill="black")
     window.after(3000, swap_bg)
 
-def bad_aswer():
-    rand_list.remove(random_choice)
-    good_answer()
+
+def wrong_answer():
+    random_select()
+
 
 def swap_bg():
     canvas.delete("black_title")
@@ -36,9 +35,7 @@ def swap_bg():
     canvas.delete("txt")
     canvas.delete("latin")
     canvas.create_text(400, 150, text="English", font=("Ariel", 50, "italic"), tags="eng", fill="white")
-    canvas.create_text(400, 263, text=f"{rand_list[1]}", font=("Ariel", 60, "bold"), tags="txt", fill="white")
-
-    # window.after_cancel(swap)
+    canvas.create_text(400, 263, text=f"{random_choice['translation']}", font=("Ariel", 60, "bold"), tags="txt", fill="white")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -58,7 +55,7 @@ right_button = Button(image=right, highlightthickness=0, command=good_answer)
 right_button.grid(column=1, row=2)
 
 wrong = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong, highlightthickness=0, command=bad_aswer)
+wrong_button = Button(image=wrong, highlightthickness=0, command=wrong_answer)
 wrong_button.grid(column=0, row=2)
 
 
